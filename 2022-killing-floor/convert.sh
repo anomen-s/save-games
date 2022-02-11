a2fc041cb5d0d67155140821d331a456828f9d82
anomen <ludek.h@gmail.com>
2022-02-12 00:53:48 +0100

    2022-killing-floor

--- 2022-killing-floor/convert.sh
+++ 2022-killing-floor/convert.sh
@@ -4,7 +4,7 @@ for FI in *.bmp
 do
  FO="$(date -r "$FI" '+%Y%m%d_%H%M').jpg"
  echo "$FI -> $FO"
- convert "$FI" -quality 90 -scale 800x500 "$FO"
+ convert "$FI" -quality 90 -scale 800x-1 "$FO"
  touch -r "$FI" "$FO"
  mv "$FI" "converted-$FI"
 done
