bed9679385ca00b6ab0eaf38889bfae7b7cb2e52
anomen <ludek.h@gmail.com>
2025-01-12 00:10:11 +0100

    shrink.sh update

--- shrink.sh
+++ shrink.sh
@@ -4,17 +4,19 @@
 W=800
 Q=80
 
-for FORMAT in bmp tga pcx png
+mkdir -p target
+
+for FORMAT in bmp tga pcx png jpg
 do
  if ls *.$FORMAT > /dev/null 2> /dev/null
  then
   for FI in *.$FORMAT
   do
-   FO="${FI%.$FORMAT}.jpg"
+   FO="target/${FI%.$FORMAT}.jpg"
    echo "$FI -> $FO"
    magick "$FI" -quality "$Q" -scale "$W"x-1 "$FO"
    touch -r "$FI" "$FO"
-   mv "$FI" "converted-$FI"
+   #mv "$FI" "converted-$FI"
   done
  fi
 done
